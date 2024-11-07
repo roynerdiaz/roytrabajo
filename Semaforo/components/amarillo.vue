875d5b435ff6384850849aa62a2f89a549f81e12
roynerdiaz <rdiazlicona94@correo.unicordoba.edu.co>
2024-11-07 02:05:08 -0500

    Add files via upload

---
+++ Semaforo/components/amarillo.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="amarillo" :class="{ 'onamarillo': isOn }"></div>
+</template>
+
+<script>
+export default {
+  props: ['isOn']
+}
+</script>
+  
+<style>
+.amarillo {
+  width: 70px;
+  height: 70px;
+  border-radius: 50%;
+  margin-bottom: 10px;
+  background-color: yellow;
+}
+
+.onamarillo {
+  background-color: rgba(255, 255, 0, 0.2);
+}
+</style>
